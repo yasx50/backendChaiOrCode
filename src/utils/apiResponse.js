e8d7b2bed83ee78d0480980c4201103ac2e6068b
yasx50 <yasxcode@gmail.com>
2024-12-01 20:13:53 +0530

    1 dec

--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -7,7 +7,7 @@ class apiResponse{
         this.statusCode = statusCode
         this.data=data
         this.message = message
-        this.success = statusCode <400
+        this.success = statusCode < 400
 
     }
 }
